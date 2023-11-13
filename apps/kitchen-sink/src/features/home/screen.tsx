cfd9a575e9873db6556403e75ba4a412a6fd02f4
benjamin <benschac@gmail.com>
2023-11-13 10:31:11 -0500

    base skeleton component setup

--- apps/kitchen-sink/src/features/home/screen.tsx
+++ apps/kitchen-sink/src/features/home/screen.tsx
@@ -184,6 +184,7 @@ const demos = [
       { title: 'LinearGradient', route: '/demo/linear-gradient' },
       { title: 'Separator', route: '/demo/separator' },
       { title: 'Square + Circle', route: '/demo/shapes' },
+      { title: 'Skeleton', route: '/demo/skeleton' },
     ],
   },
 
